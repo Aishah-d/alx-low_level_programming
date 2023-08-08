d23f149192fe6f8960d136167dbc41a742e9b50e
root <akindeleaisha20@gmail.com>
2023-08-08 03:14:26 -0700

    100-argstostr.c

--- 0-create_array.c/100-argstostr.c
+++ 0-create_array.c/100-argstostr.c
@@ -23,3 +23,25 @@ char *argstostr(int ac, char **av)
 		len++;
 	}
 
+	str = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	
+	return (str);
+}
+
+
